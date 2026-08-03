a4f81fe91898dfce1a0b992b5f8d62bcce42a0f9
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-02 19:56:30 -0700

    [Copyright] fix: Name this package in every header.

--- tests/unit/test_template_info.py
+++ tests/unit/test_template_info.py
@@ -1,8 +1,10 @@
-# This file is part of the Project Template package.
+#
+# This file is part of the Valkyrja Ruff package.
 #
 # Copyright (c) 2016-present Melech Mizrachi
 #
 # Released under the MIT License. See LICENSE.md for details.
+#
 
 """Tests for TemplateInfo.
 
